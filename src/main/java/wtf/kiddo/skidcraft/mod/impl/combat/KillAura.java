00c28b804adf358f3a9be29a5616dbf3a322a8a5
Insane1337 <3416646357@qq.com>
2023-12-03 22:20:34 +0800

    低雅targethud

--- src/main/java/wtf/kiddo/skidcraft/mod/impl/combat/KillAura.java
+++ src/main/java/wtf/kiddo/skidcraft/mod/impl/combat/KillAura.java
@@ -2,13 +2,20 @@ package wtf.kiddo.skidcraft.mod.impl.combat;
 
 import me.bush.eventbus.annotation.EventListener;
 import net.minecraft.src.*;
+import org.lwjgl.opengl.GL11;
 import wtf.kiddo.skidcraft.event.LBUpdateEvent;
 import wtf.kiddo.skidcraft.event.PacketEvent;
+import wtf.kiddo.skidcraft.event.Render2DEvent;
 import wtf.kiddo.skidcraft.mod.Category;
 import wtf.kiddo.skidcraft.mod.Mod;
+import wtf.kiddo.skidcraft.utils.RenderUtils;
 import wtf.kiddo.skidcraft.utils.RotationUtils;
 import wtf.kiddo.skidcraft.value.impl.BooleanValue;
 import wtf.kiddo.skidcraft.value.impl.NumberValue;
+
+import java.awt.*;
+
+import static wtf.kiddo.skidcraft.utils.RenderUtils.getNextPostion;
 
 
 public final class KillAura extends Mod {
@@ -19,9 +26,10 @@ public final class KillAura extends Mod {
 
     private static final BooleanValue silentRotationValue = new BooleanValue("SilentRotation",true);
     private static final BooleanValue autoblockValue = new BooleanValue("AutoBlock",true);
+
+    private final static BooleanValue targethud = new BooleanValue("TargetHUD",true);
     private static final NumberValue<Float> rangeValue = new NumberValue<>("Range", 4.0f, 1.0f, 6.0f, 0.1f);
     float[] rotation = new float[2];
-
     public KillAura() {
         super("KillAura", Category.Combat);
     }
@@ -44,6 +52,9 @@ public final class KillAura extends Mod {
                     return;
                 if(((EntityLiving) en).isInvisible() && !invisibles.getValue())
                     return;
+
+
+
                 if(autoblockValue.getValue() && !((EntityLiving) en).isDead && mc.thePlayer.getHeldItem() != null && mc.thePlayer.getHeldItem().getItem() instanceof ItemSword ){
                     // https://github.com/BapeDeveloperTeam/Bape-Opensource/blob/main/src/main/java/mc/bape/module/blatant/Killaura.java
                     mc.thePlayer.getCurrentEquippedItem().useItemRightClick(mc.theWorld, mc.thePlayer);
@@ -69,6 +80,41 @@ public final class KillAura extends Mod {
             ((Packet10Flying) packet).yaw = rotation[0];
             ((Packet10Flying) packet).pitch = rotation[1];
 
+        }
+    }
+    int animWidth;
+    float f6;
+
+    @EventListener
+    public void onR2D(final Render2DEvent event) {
+        for (Object en : mc.theWorld.loadedEntityList) {
+            if (mc.thePlayer.getDistanceToEntity((Entity) en) < rangeValue.getValue() && en != mc.thePlayer && en instanceof EntityLiving && targethud.getValue()) {
+                ScaledResolution sr = new ScaledResolution(mc);
+                final int n4 = sr.getScaledWidth() / 2 + 150;
+                final int n5 = sr.getScaledHeight() / 2 + 100;
+                int n15 = -1;
+
+                RenderUtils.drawRect1(n4 - 1.0f, n5 + 4.0f, 105.0f, 45.0f, new Color(0, 0, 0, 100));
+
+                mc.fontRenderer.drawString(((EntityLiving) en).getEntityName(), n4 + 22, n5 + 6,-1);
+                GL11.glPushMatrix();
+                GL11.glTranslatef(n4, n5, 1.0f);
+                GL11.glScalef(2.0f, 2.0f, 2.0f);
+                GL11.glTranslatef((float)(-n4), (float)(-n5), (float)1.0f);
+                mc.fontRenderer.drawString(String.valueOf(new StringBuilder().append((double)Math.round((double)(((EntityLiving) en).getHealth() / 2.0f) * 10.0) / 10.0).append(" \u2764")), n4 + 10, n5 + 9, n15);
+                GL11.glPopMatrix();
+                GL11.glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+                f6 = 105.0f * ((EntityLiving) en).getHealth() / ((EntityLiving) en).getMaxHealth();
+                if ((float)animWidth > f6) {
+                    animWidth = getNextPostion(animWidth, (int)f6, 100.0);
+                }
+                if ((float)animWidth < f6) {
+                    animWidth = getNextPostion(animWidth, (int)f6, 100.0);
+                }
+
+                RenderUtils.drawRect1(n4 + -1, n5 + 46, animWidth, 3.0f,Color.GREEN);
+
+            }
         }
     }
 
--- src/main/java/wtf/kiddo/skidcraft/utils/RenderUtils.java
+++ src/main/java/wtf/kiddo/skidcraft/utils/RenderUtils.java
@@ -7,6 +7,11 @@ import net.minecraft.src.RenderManager;
 import net.minecraft.src.Tessellator;
 import org.lwjgl.opengl.GL11;
 
+import java.awt.*;
+
+import static org.lwjgl.opengl.GL11.glDisable;
+import static org.lwjgl.opengl.GL11.glEnable;
+
 public class RenderUtils {
     private static Minecraft mc = Minecraft.getMinecraft();
     public static void drawTracerLine(double x, double y, double z, float red, float green, float blue, float alpha, float lineWdith) {
@@ -32,6 +37,59 @@ public class RenderUtils {
         GL11.glDisable(GL11.GL_BLEND);
         GL11.glPopMatrix();
     }
+
+    public static void drawRect1(float f, float f2, float f3, float f4, Color color) {
+        RenderUtils.drawRect(f, f2, f + f3, f2 + f4, color.getRGB());
+    }
+
+    public static void drawRect(float left, float top, float right, float bottom, int col1) {
+        final float f = (col1 >> 24 & 0xFF) / 255.0F, // @off
+                f1 = (col1 >> 16 & 0xFF) / 255.0F,
+                f2 = (col1 >> 8 & 0xFF) / 255.0F,
+                f3 = (col1 & 0xFF) / 255.0F; // @on
+
+        glEnable(3042);
+        glDisable(3553);
+        GL11.glBlendFunc(770, 771);
+        glEnable(2848);
+
+        GL11.glPushMatrix();
+        GL11.glColor4f(f1, f2, f3, f);
+        GL11.glBegin(7);
+        GL11.glVertex2d(right, top);
+        GL11.glVertex2d(left, top);
+        GL11.glVertex2d(left, bottom);
+        GL11.glVertex2d(right, bottom);
+        GL11.glEnd();
+        GL11.glPopMatrix();
+
+        glEnable(3553);
+        glDisable(3042);
+        glDisable(2848);
+        GL11.glEnable(GL11.GL_TEXTURE_2D);
+        GL11.glDisable(GL11.GL_BLEND);
+        GL11.glColor4f(1, 1, 1, 1);
+    }
+
+
+    public static int getNextPostion(int n, int n2, double d) {
+        int n3 = n;
+        if (n3 < n2) {
+            int n4 = (int)((double)(n2 - n3) / d);
+            if (n4 < 1) {
+                n4 = 1;
+            }
+            n3 += n4;
+        } else if (n3 > n2) {
+            int n5 = (int)((double)(n3 - n2) / d);
+            if (n5 < 1) {
+                n5 = 1;
+            }
+            n3 -= n5;
+        }
+        return n3;
+    }
+
     private static void drawOutlinedBoundingBox(AxisAlignedBB par1AxisAlignedBB){
         Tessellator tessellator = Tessellator.instance;
         tessellator.startDrawing(3);
